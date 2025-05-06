cb471b1a5fd2aa75b713f082b1fb02b92a4c1404
miskogepa <milovan.jankovic50m@gmail.com>
2025-05-06 15:02:17 +0200

    feat: implement POST endpoint for adding tea data

--- express/index.js
+++ express/index.js
@@ -3,10 +3,23 @@ import express from "express";
 const app = express();
 
 const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("misko programer!!!"); //menjamo poruku
-});
+// array za teaData 
+let teaData = [];
+let nextId = 1;
+
+app.post("/cajevi", (req, res) => {
+    const { naziv, tip, cena } = req.body;
+    const noviCaj = {
+        id: nextId++,
+        naziv,
+        cena
+    };
+    teaData.push(noviCaj);
+res.status(201).send(noviCaj);
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}...`);
